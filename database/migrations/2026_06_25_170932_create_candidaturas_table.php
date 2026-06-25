528cbea5827f03384d80429db597cdb8b563ce40
richard <richardbryanpereirarosa06@gmail.com>
2026-06-25 14:22:08 -0300

    Migrations Finalizado

--- database/migrations/2026_06_25_170932_create_candidaturas_table.php
+++ database/migrations/2026_06_25_170932_create_candidaturas_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('candidaturas', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('vagas_id');
+            $table->string('usuarios_id');
         });
     }
 
